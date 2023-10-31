790185aaa477ae15d415112104c32f940251582d
Bruno Bandelli <brunobandelli@gmail.com>
2023-10-31 15:22:06 -0300

    metodos introspecção de propriedades Objetos

--- metodosPreDefinidos_IntrospeccaoDePropriedades.js
+++ metodosPreDefinidos_IntrospeccaoDePropriedades.js
@@ -4,3 +4,135 @@
 Métodos de Introspecção.
 Esses métodos ajudam a obter informações sobre propriedades de um objeto.
 */
+
+
+/*"Chaves do Objeto"*/
+/*
+Object.keys(objeto)
+Retorna um array das chaves (nomes das propriedades) de um objeto.
+*/
+
+const pessoa = {
+    nome: "João",
+    idade: 30,
+    profissao: "Programador"
+};
+  
+const chaves = Object.keys(pessoa);
+console.log(chaves); // ["nome", "idade", "profissao"]
+  
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("--------------------------------------------------------------------------------------------------------.keys")
+
+/*"Valores do Objeto" */
+/*
+Object.values(objeto)
+Retorna um array dos valores das propriedades de um objeto.
+*/
+
+const carro = {
+    marca: "Toyota",
+    modelo: "Camry",
+    ano: 2022
+};
+  
+const valores = Object.values(carro);
+console.log(valores); // ["Toyota", "Camry", 2022]
+  
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("------------------------------------------------------------------------------------------------------.values")
+
+/*"Entradas do Objeto"*/
+/*
+Object.entries(objeto)
+Retorna um array de pares chave-valor das propriedades de um objeto.
+*/
+
+const fruta = {
+    nome: "Maçã",
+    cor: "Vermelha",
+    sabor: "Doce"
+};
+  
+const entradas = Object.entries(fruta);
+console.log(entradas); // [["nome", "Maçã"], ["cor", "Vermelha"], ["sabor", "Doce"]]
+  
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("-----------------------------------------------------------------------------------------------------.entries")
+
+/*"Obter Nomes de Propriedade do Objeto" */
+/*
+Object.getOwnPropertyNames(objeto)
+Retorna um array com os nomes das propriedades próprias do objeto (não inclui propriedades herdadas).
+*/
+
+function Animal(nome) {
+    this.nome = nome;
+}
+  
+Animal.prototype.tipo = "Desconhecido";
+  
+const gato = new Animal("Whiskers");
+  
+const propriedades = Object.getOwnPropertyNames(gato);
+console.log(propriedades); // ["nome"]
+  
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("-----------------------------------------------------------------------------------------.getOwnPropertyNames")
+
+/*"Obter Símbolos de Propriedade do Objeto" */
+/*
+Object.getOwnPropertySymbols(objeto)
+Retorna um array com os símbolos das propriedades próprias do objeto (não inclui propriedades de texto).
+*/
+
+const simbolo1 = Symbol("descricao");
+const simbolo2 = Symbol("outraDescricao");
+
+const objeto = {
+  [simbolo1]: "Valor 1",
+  [simbolo2]: "Valor 2"
+};
+
+const simbolos = Object.getOwnPropertySymbols(objeto);
+console.log(simbolos); // [Symbol(descricao), Symbol(outraDescricao)]
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("---------------------------------------------------------------------------------------.getOwnPropertySymbols")
+
+/*"Obter Descritor de Propriedade do Objeto" */
+/*
+Object.getOwnPropertyDescriptor(objeto, propriedade)
+Retorna um objeto com informações sobre a propriedade, como se ela é gravável, enumerável e configurável.
+*/
+
+const pessoaDescriptor = {
+    nome: "Maria",
+    idade: 25
+  };
+  
+  const descritor = Object.getOwnPropertyDescriptor(pessoaDescriptor, "nome");
+  console.log(descritor);
+  /*
+  {
+    value: "Maria",
+    writable: true,
+    enumerable: true,
+    configurable: true
+  }
+  */
+  
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("------------------------------------------------------------------------------------.getOwnPropertyDescriptor")
--- metodosPreDefinidos_ManipulacaoDePropriedades.js
+++ metodosPreDefinidos_ManipulacaoDePropriedades.js
@@ -6,9 +6,10 @@ Esses métodos estão relacionados à adição, remoção ou alteração de prop
 */
 
 
-//Exemplo 1: Object.assign(objDestino, obj1, obj2, ...)
-//Copia propriedades de objetos de origem para um objeto de destino.
+/*"Atribuir"*/
 /*
+Object.assign(objDestino, obj1, obj2, ...)
+Copia propriedades de objetos de origem para um objeto de destino.
 Semântica: Copia as propriedades dos objetos de origem para o objeto de destino.
 */
 
@@ -23,11 +24,12 @@ console.log(destino); // { a: 1, b: 3, c: 4 }
 
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 1")
+console.log("------------------------------------------------------------------------------------------------------.assign")
 
-//Exemplo 2: Object.create(proto, propriedades)
-//Criar um novo objeto com um protótipo e propriedades adicionais opcionais.
+/*"Criar"*/
 /*
+Object.create(proto, propriedades)
+Criar um novo objeto com um protótipo e propriedades adicionais opcionais.
 Semântica: Cria um novo objeto com um protótipo especificado (proto) e propriedades opcionais (propriedades).
 */
 
@@ -45,11 +47,12 @@ console.log(gato.som); // "Miado"
   
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 2")
+console.log("------------------------------------------------------------------------------------------------------.create")
 
-//Exemplo 3: Object.defineProperty(objeto, propriedade, descritor)
-//Define uma nova propriedade com configurações específicas em um objeto.
+/*"DefinirPropriedade"*/
 /*
+Object.defineProperty(objeto, propriedade, descritor)
+Define uma nova propriedade com configurações específicas em um objeto.
 Semântica: Define uma nova propriedade no objeto com um nome específico e configurações específicas definidas no descritor.
 */
 
@@ -65,20 +68,86 @@ produto.nome = "Calça"; // Isso não terá efeito devido à propriedade não se
 
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 3")
+console.log("----------------------------------------------------------------------------------------------.defineProperty")
 
-//Exemplo 4: 
+/*"DefinirPropriedades"*/
+/*
+Object.defineProperties(objeto, descritores)
+Definir várias propriedades em um objeto com configurações específicas.
+Semântica: Define várias propriedades no objeto com base nos descritores fornecidos.
+*/
+
+const livro = {};
+
+Object.defineProperties(livro, {
+  título: { value: "Aventuras de Sherlock Holmes", writable: true },
+  autor: { value: "Arthur Conan Doyle", writable: true }
+});
+
+console.log(livro.título); // "Aventuras de Sherlock Holmes"
+console.log(livro.autor); // "Arthur Conan Doyle"
+
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 4")
+console.log("--------------------------------------------------------------------------------------------.defineProperties")
 
-//Exemplo 5: 
+/*"PrevenirExtensões"*/
+/*
+Object.preventExtensions(objeto)
+Impedir a adição de novas propriedades a um objeto existente. No entanto, ainda é possível modificar as propriedades existentes.
+Semântica: Impede a adição de novas propriedades a um objeto, mas permite a modificação das propriedades existentes.
+*/
+
+const objeto = { a: 1, b: 2 };
+Object.preventExtensions(objeto);
+
+objeto.c = 3; // Isso não funcionará
+console.log(objeto.c)
+objeto.a = 42; // Isso funciona
+console.log(objeto.a)
+
+
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 5")
+console.log("-------------------------------------------------------------------------------------------.preventExtensions")
 
-//Exemplo 6: 
+/*"Selar"*/
+/*
+Object.seal(objeto)
+Impedir a adição e remoção de propriedades, mas permitir a modificação das propriedades existentes.
+Semântica: Impede a adição e remoção de propriedades, mas permite a modificação das propriedades existentes.
+*/
+
+const pessoa = { nome: "Maria", idade: 30 };
+Object.seal(pessoa);
+
+delete pessoa.nome; // Isso não funcionará
+console.log(pessoa.nome)
+pessoa.endereco = "123 Rua Principal"; // Isso não funcionará
+console.log(pessoa.endereco)
+pessoa.idade = 35; // Isso funciona
+console.log(pessoa.idade)
+
 
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 6")
+console.log("--------------------------------------------------------------------------------------------------------.seal")
+/*"Congelar"*/
+/*
+Object.freeze(objeto)
+Impedir qualquer modificação nas propriedades de um objeto, tornando-o imutável.
+Semântica: Impede qualquer modificação nas propriedades de um objeto, tornando-o imutável.
+*/
+
+const dados = { altura: 180, peso: 75 };
+Object.freeze(dados);
+
+delete dados.altura; // Isso não funcionará
+console.log(dados.altura)
+dados.peso = 80; // Isso não funcionará
+console.log(dados.peso)
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+console.log("------------------------------------------------------------------------------------------------------.freeze")
