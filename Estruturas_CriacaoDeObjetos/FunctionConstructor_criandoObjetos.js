f51df8bc3f2b248e6ec7dd4cb146d819004da33b
Bruno Bandelli <brunobandelli@gmail.com>
2023-11-24 23:37:49 -0300

    criação dinaminca de classes

--- Estruturas_CriacaoDeObjetos/FunctionConstructor_criandoObjetos.js
+++ Estruturas_CriacaoDeObjetos/FunctionConstructor_criandoObjetos.js
@@ -125,23 +125,138 @@ Para visualizá-los, é necessário acessá-los através do prototype da classe,
 /*------------------------------------------------------------------------------------------------------------------------*/
 console.log("-----------------------------------------------------------------------------------------------------Exemplo 2")
 
-//Exemplo 3: 
+//Exemplo 3: Criando classes de forma dinâmica
+
+/*
+O uso dinâmico de classes e construtores em JavaScript, é mais comum em situações avançadas, 
+especialmente quando se lida com bibliotecas ou frameworks que precisam criar abstrações complexas de forma dinâmica.
+Veja um exemplo abaixo:
+*/
+
+/*Exemplo: 3-A*/
+
+function criarClasse(nomeDaClasse) {
+  return class {
+      constructor(parametro) {
+          this.nome = parametro;
+      }
+
+      exibirNome() {
+          console.log(`Nome da ${nomeDaClasse}: ${this.nome}`);
+      }
+  };
+}
+
+const MinhaClasseDinamica = criarClasse("Instância Dinâmica");
+const instancia = new MinhaClasseDinamica("Exemplo");
+instancia.exibirNome(); //Output: Nome da Instância Dinâmica: Exemplo
+
+
+/*
+Vamos analisar esse trecho de código passo a passo:
+
+const MinhaClasseDinamica = criarClasse("Instância Dinâmica");
+const instancia = new MinhaClasseDinamica("Exemplo");
+instancia.exibirNome();
+
+Passo a passo:
+
+*const MinhaClasseDinamica = criarClasse("Instância Dinâmica");
+Aqui, você está chamando a função criarClasse e passando o argumento "Instância Dinâmica". 
+Essa função retorna uma classe dinâmica (uma função construtora). 
+Então, MinhaClasseDinamica se torna uma referência para essa classe.
+
+*const instancia = new MinhaClasseDinamica("Exemplo");
+Aqui, você está criando uma nova instância da classe dinâmica usando o operador new. 
+Essa instância é armazenada na variável instancia. Você também está passando o argumento "Exemplo" para o construtor da classe.
+
+*instancia.exibirNome();
+Agora, você está chamando o método exibirNome() da instância criada. Dentro desse método, 
+há uma instrução de log que exibe uma mensagem contendo o nome da classe (passado durante a criação da classe) e 
+o nome passado durante a criação da instância.
+*/
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 3")
+console.log("-----------------------------------------------------------------------------------------------------Exemplo 3-A")
 
-//Exemplo 4: 
+//Exemplo 3-B*/
+
+/*
+Vamos supor que você tenha um sistema de plugins, e cada plugin pode ter uma lógica específica. 
+Você poderia usar uma função para dinamicamente criar classes de plugins com base nas configurações fornecidas. 
+Isso permite que você adicione novos plugins sem ter que definir explicitamente uma classe para cada um.
+*/
+
+// Função que cria dinamicamente classes de plugins
+function criarClasseDePlugin(nomeDoPlugin) {
+  return class {
+      constructor(config) {
+          this.nome = nomeDoPlugin;
+          this.config = config;
+      }
+
+      executar() {
+          console.log(`Executando ${this.nome} com configurações:`, this.config);
+      }
+  };
+}
+
+// Uso da função para criar classes de plugins dinamicamente
+const PluginA = criarClasseDePlugin("PluginA");
+const PluginB = criarClasseDePlugin("PluginB");
+
+// Instâncias de plugins
+const instanciaPluginA = new PluginA({ opcaoA: true });
+const instanciaPluginB = new PluginB({ opcaoB: "valor" });
+
+// Execução dos plugins
+instanciaPluginA.executar(); // Saída: Executando PluginA com configurações: { opcaoA: true }
+instanciaPluginB.executar(); // Saída: Executando PluginB com configurações: { opcaoB: "valor" }
+
+/*
+Neste exemplo, a função criarClasseDePlugin retorna uma classe para cada tipo de plugin, 
+e você pode criar instâncias dessas classes dinamicamente com base nos nomes dos plugins e suas configurações. 
+Esse é um caso simplificado, mas em sistemas mais complexos, a criação dinâmica de classes pode ser uma ferramenta poderosa.
+
+O objetivo desse padrão é permitir a criação de classes e instâncias de forma dinâmica, 
+o que pode ser útil em certos cenários, como sistemas de plugins ou extensões, 
+onde você pode adicionar novos tipos de plugins sem a necessidade de definir explicitamente uma nova classe para cada um.
+*/
+
+
+/*Passo a passo:
+
+*const PluginA = criarClasseDePlugin("PluginA"); e const PluginB = criarClasseDePlugin("PluginB");
+Aqui, você está usando a função criarClasseDePlugin para criar dinamicamente duas classes de plugins: PluginA e PluginB.
+PluginA e PluginB são agora referências para classes geradas pela função criarClasseDePlugin, 
+cada uma representando um tipo diferente de plugin.
+
+*const instanciaPluginA = new PluginA({ opcaoA: true });
+Agora, você está criando uma instância da classe PluginA utilizando o operador new.
+A função construtora da classe é chamada, e você está passando um objeto de configuração { opcaoA: true } como argumento para o construtor.
+A instância resultante é armazenada na variável instanciaPluginA.
+
+*const instanciaPluginB = new PluginB({ opcaoB: "valor" });
+Da mesma forma, você está criando uma instância da classe PluginB com uma configuração diferente { opcaoB: "valor" }.
+A instância resultante é armazenada na variável instanciaPluginB.
+
+Resumindo, 
+as linhas mencionadas estão criando dinamicamente duas classes de plugins (PluginA e PluginB) usando a função criarClasseDePlugin, e, 
+em seguida, estão instanciando essas classes para criar objetos específicos (instanciaPluginA e instanciaPluginB) com diferentes configurações.
+
+*/
+
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 4")
+console.log("-----------------------------------------------------------------------------------------------------Exemplo 3-B")
 
 //Exemplo 5: 
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 5")
+console.log("-----------------------------------------------------------------------------------------------------Exemplo 4")
 
 //Exemplo 6: 
 
 
 /*------------------------------------------------------------------------------------------------------------------------*/
-console.log("-----------------------------------------------------------------------------------------------------Exemplo 6")
+console.log("-----------------------------------------------------------------------------------------------------Exemplo 5")
