327ac925e9c0aa822ab2a69ea4ab74c872c2d0e5
Bruno Bandelli <brunobandelli@gmail.com>
2023-12-05 19:36:24 -0300

    COMPOSIÇÃO VS HERANÇA

--- Estruturacoes_ConceitosDePOO/ComposicaoPOO.js
+++ Estruturacoes_ConceitosDePOO/ComposicaoPOO.js
@@ -1,13 +1,18 @@
 /*COMPOSIÇÃO*/
 
 /*
+INTRODUÇÃO:
 A composição promove a reutilização de código e ajuda a evitar os problemas associados à herança, 
 como o acoplamento rígido e a hierarquia de classes complexa.
+A composição é uma técnica poderosa em POO que ajuda a criar código modular, 
+flexível e de fácil manutenção, e é frequentemente preferida sobre a herança quando se trata de projetar sistemas complexos.
+
+SIMPLIFICANDO:
 
 A composição em programação é como construir algo maior (uma aplicação) combinando diferentes "blocos" (classes),
 como se fosse legos, graças a sua aborgadem modular.
 
-
+------------------------------------------------------------------------------------------------------------------------
 
 ANALOGIA:
 Construindo uma Casa com LEGO
@@ -46,8 +51,9 @@ reutilizável e fácil de atualizar, assim como a construção com LEGO permite 
 
 /*---------------------------------------------------------------------------------------------------------------------*/
 
-/*Exemplos:*/
+/*EXEMPLOS:*/
 
+//Exemplo 1: "Compondo um carro".
 
 // Definindo a classe Radio
 class Radio {
@@ -79,7 +85,7 @@ class Motor {
   }
 }
 
-// Atualização da classe Carro para incluir um rádio
+// Compondo a classe Carro com as classes anteriores
 class Carro {
   constructor(marca, modelo) {
     this.marca = marca;
@@ -101,20 +107,29 @@ class Carro {
   }
 }
 
-// Exemplo de uso
+// Criando uma instância da classe Carro e utilizando os métodos de composição.
 const meuCarro = new Carro("Toyota", "Corolla");
+//ligando o motor e o rádio
 meuCarro.ligarCarro();
+//desligando o motor e o rádio
 meuCarro.desligarCarro();
 
 
 
 /*
-A composição é uma técnica poderosa em POO que ajuda a criar código modular, 
-flexível e de fácil manutenção, e é frequentemente preferida sobre a herança quando se trata de projetar sistemas complexos.
+OBS:
+Enquanto a herança estabelece uma relação "is-a"(é -um) entre classes, 
+indicando que uma classe filha herda características da classe pai (por exemplo, Carro é um Veículo), 
+a composição cria uma relação "has-a"(tem-um), 
+onde uma classe contém instâncias de outras classes (por exemplo, Carro tem um Motor). 
+A escolha entre herança e composição depende dos requisitos específicos do projeto e do design desejado.
 */
 
+
+
 /*
-Aqui estão alguns pontos-chave sobre composição:
+PONTOS CHAVES SOBRE COMPOSIÇÃO:
+Aqui estão alguns pontos-chave sobre composição.
 
 Objetos Componentes:
 Na composição, você define objetos que contêm outros objetos como seus componentes. 
@@ -136,4 +151,54 @@ A composição ajuda a evitar o acoplamento rígido,
 um problema comum na herança. Quando você herda de uma classe, 
 qualquer alteração na classe pai pode afetar todas as classes filhas. 
 Com a composição, as alterações nos objetos componentes geralmente têm um impacto limitado.
- */
+*/
+
+
+
+/*
+COPOSIÇÃO VS HERANÇA:
+Herança e composição são duas abordagens diferentes na construção de relacionamentos entre classes em POO.
+Aqui estão as principais diferenças entre elas:
+
+Herança:
+Relacionamento "É-UM" (is-a): Na herança, uma classe herda características de outra classe, 
+indicando um relacionamento "é-um". Por exemplo, se temos uma classe Carro que herda de Veículo, 
+estamos dizendo que um carro é um tipo específico de veículo.
+
+Reutilização de Código: A herança permite reutilizar código de uma classe pai na classe filha.
+A classe filha herda métodos e propriedades da classe pai.
+
+Hierarquia de Classes: A herança cria uma hierarquia de classes, 
+onde as classes filhas podem herdar de múltiplas classes, formando uma cadeia hierárquica.
+
+Composição:
+Relacionamento "Tem-um" (has-a): Na composição, uma classe é composta por instâncias de outras classes, 
+indicando um relacionamento "tem-um". Por exemplo, se temos uma classe Carro que tem uma instância de Motor, 
+estamos dizendo que um carro tem um motor.
+
+Flexibilidade: A composição é mais flexível do que a herança. 
+Pode-se alterar o comportamento de uma classe composta mais facilmente, adicionando ou removendo componentes.
+
+Evita Problemas de Herança: Herança pode levar a problemas como a herança múltipla, 
+onde uma classe herda de várias classes, tornando o código mais complexo e difícil de manter. 
+Composição evita esses problemas.
+
+Acoplamento Fraco: Composição muitas vezes resulta em acoplamento mais fraco entre as classes, 
+pois uma classe não precisa conhecer os detalhes internos de outra classe, apenas interage com sua interface pública.
+
+Escolha entre Herança e Composição:
+Use Herança quando:
+
+Existe uma clara relação "é-um" entre as classes.
+Há uma necessidade real de reutilizar o código da classe pai.
+A hierarquia de classes faz sentido no domínio do problema.
+Use Composição quando:
+
+A relação "tem-um" é mais apropriada.
+É necessária uma maior flexibilidade.
+Deseja-se evitar problemas associados à herança, como a herança múltipla.
+Em muitos casos, uma combinação de herança e composição pode ser a abordagem mais eficaz, 
+dependendo dos requisitos específicos do projeto. 
+É importante entender as vantagens e desvantagens de cada abordagem para tomar decisões informadas durante 
+o design do sistema.
+*/
